3f978daf724a9ccbadf7c1e0dd621df75ef70764
giannzi <giannazia3@gmail.com>
2025-06-13 09:13:48 -1000

    activity 16

--- Activity-16/scripts/script.js
+++ Activity-16/scripts/script.js
@@ -12,6 +12,8 @@ document.getElementById('randomColor').addEventListener('click', function() {
 
 function displayColor(color) {
     document.getElementById('displayArea').style.backgroundColor = color;
+    document.body.style.backgroundColor = color;
+    document.getElementById('colorValue').textContent = color;
 }
 
 function addToHistory(color) {
